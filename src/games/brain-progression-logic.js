05f3cb78fa25f42a34e611ce4f2b6f1a6fae1eca
Nikitereh <nikita607693@yandex.ru>
2022-03-01 21:18:41 +0300

    make logic for progressin game

---
+++ src/games/brain-progression-logic.js
@@ -0,0 +1,32 @@
+import func from '../index.js';
+import getRandomNumber from '../getRandomNumber.js';
+
+const rules = 'What number is missing in the progression?';
+
+const createProgression = (progressionStart, progressionStep, progressionLength) => {
+  const arr = [];
+  for (let i = 0; i <= progressionLength; i += 1) {
+    const result = progressionStart + (i * progressionStep);
+    arr.push(result);
+  }
+  return arr;
+};
+
+const check = () => {
+  const firstNumber = getRandomNumber(1, 100);
+  const step = getRandomNumber(2, 10);
+  const randomLength = getRandomNumber(5, 10);
+
+  const progression = createProgression(firstNumber, step, randomLength);
+  const randomIndex = getRandomNumber(0, progression.length - 1);
+
+  const correctAnswer = `${progression[randomIndex]}`;
+  progression[randomIndex] = '..';
+  const question = progression.join(' ');
+
+  return [question, correctAnswer];
+};
+
+const brainProgression = () => func(rules, check);
+
+export default brainProgression;
